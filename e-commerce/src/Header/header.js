6cb881807a09f4728ed1c50675d1dfdeacf31bfb
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2019-11-01 11:29:29 +0530

    added notification functionality

--- e-commerce/src/Header/header.js
+++ e-commerce/src/Header/header.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
 import styles from './header.module.scss';
-const Header=(props)=>{
-    return(
-        <div className={styles.header}>
-            <div className={styles.projectTitle}>
-                <span>Shopify</span>
-            </div>
-            <div className={styles.navigation}>
-                <a href="#">Orders</a>
-                <a href="#">Whishlist</a>
-                <a href="#">Cart</a>
-            </div>
-        </div>
-    )
-}
+class Header extends Component{
 
-export default Header;
+    render(){
+        const notificationWishListClasses=[styles.notificationHide];
+        if(this.props.wishlist.length>0)
+        {
+            notificationWishListClasses.push(styles.notification);
+            
+        }
+        return(
+            <div className={styles.header}>
+                <div className={styles.projectTitle}>
+                    <span>Shopify</span>
+                </div>
+                <div className={styles.navigation}>
+                    <a href="#">Orders</a>
+                    <a href="#" className={styles.hasNotification}>Wishlist
+                        <span className={notificationWishListClasses.join(' ')}>{this.props.wishlist.length}</span>
+                    </a>
+                    <a href="#" className={styles.hasNotification}>Cart
+                        {/* <span className={styles.notification}>1</span> */}
+                    </a>
+                </div>
+            </div>
+        )
+    }
+   
+}
+const mapStateToProps=(state)=>{
+    return{
+        wishlist:state.wishlist
+    }
+}
+export default connect(mapStateToProps,null)(Header);
